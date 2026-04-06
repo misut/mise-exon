cabcff99809e120b8d1806a05a3b61c3669ef888
misut <pkr5207@gmail.com>
2026-04-06 10:59:21 +0900

    feat: convert from asdf to vfox plugin format
    Replace shell-based asdf plugin scripts with Lua-based vfox hooks.
    asdf plugins cannot run on Windows because mise executes them via
    CreateProcessW which rejects shell scripts. vfox plugins use Lua
    which mise runs natively on all platforms.

---
+++ metadata.lua
@@ -0,0 +1,7 @@
+PLUGIN = {}
+PLUGIN.name = "exon"
+PLUGIN.version = "0.1.0"
+PLUGIN.description = "A package manager for C++"
+PLUGIN.homepage = "https://github.com/misut/exon"
+PLUGIN.license = "MIT"
+PLUGIN.minRuntimeVersion = "0.3.0"
